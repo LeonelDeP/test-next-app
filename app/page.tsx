25ed7edda788be122b7a32d6fddc935d9a5f2e40
LeonelDeP <36462344+LeonelDeP@users.noreply.github.com>
2025-12-18 17:14:54 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>🚀 Sitio de prueba Next.js</h1>
+      <p>Deploy automático en Huawei ECS</p>
+      <p>Self-hosted runner • Sin Docker • /webroot</p>
+    </main>
+  );
+}
